0c326abe36281a4cd4f2a1c71ee9d57177473a8d
av308q <angelv1901@gmail.com>
2021-07-19 17:04:13 -0400

    added meal item form

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,10 @@
+import classes from './MealItemForm.module.css';
+
+const MealsItemForm = props => {
+    return <form className={classes.form}>
+        <imput/>
+        <button>+ Add</button>
+    </form>
+};
+
+export default MealsItemForm;
