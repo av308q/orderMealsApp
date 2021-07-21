dcdebbb2133320625ede3ee3cf5062c98958a27c
av308q <angelv1901@gmail.com>
2021-07-21 12:07:10 -0400

    added input amount and other values

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,20 @@
+import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
-const MealsItemForm = props => {
-    return <form className={classes.form}>
-        <imput/>
+const MealsItemForm = (props) => {
+    return (
+    <form className={classes.form}>
+        <Input lable ="Amount" input={{
+            id: 'amount',
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultvalue: '1'
+        }}/>
         <button>+ Add</button>
     </form>
+    );
 };
 
 export default MealsItemForm;
