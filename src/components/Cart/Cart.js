02367a2e8f6b179ea38772d409aa3bb667af0a40
av308q <angelv1901@gmail.com>
2021-07-22 23:00:44 -0400

    create modal.js file and overlay added overlay on index.html

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css'
 
 const Cart = (props) => {
@@ -8,7 +9,8 @@ const Cart = (props) => {
     ))}
     </ul>
     );
-    return <div>
+    return (
+    <Modal>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -18,7 +20,8 @@ const Cart = (props) => {
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
       </dive>
-    </div>
+    </Modal>
+    );
 };
 
 export default Cart;
